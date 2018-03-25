c5d4d01a0cb88ba40547141ec3649dc81de50a81
Nupur Baghel <nupurbaghel@gmail.com>
2018-03-25 21:07:03 +0530

    added albums list app

---
+++ firstapp/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+//import App from './App';
+
+const App = () => {
+	return (
+		<View style={{ flex: 1 }}>
+			<Header headerText={'Albums'} />
+			<AlbumList />
+		</View>
+	);
+};
+
+AppRegistry.registerComponent('firstapp', () => App);
